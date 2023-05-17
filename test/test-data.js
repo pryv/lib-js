72bcf8e0f75204872658013a7139a728d16648e0
Perki <perki@pryv.com>
2023-05-17 14:17:15 +0200

    Updated testData to support dnsless and custom urls

--- test/test-data.js
+++ test/test-data.js
@@ -4,9 +4,20 @@
  */
 const superagent = require('superagent');
 
-const username = 'jslibtest5';
-const serviceInfoUrl = 'https://reg.pryv.me/service/info';
-// const serviceInfoUrl = 'https://l.rec.la:4443/reg/service/info';
+const username = 'jslibtest6';
+
+// default testing on Pryv.me
+let serviceInfoUrl = 'https://reg.pryv.me/service/info';
+let coreDnsLessUrl = null;
+
+if (process.env.TEST_PRYVLIB_SERVICEINFO_URL != null) {
+  serviceInfoUrl = process.env.TEST_PRYVLIB_SERVICEINFO_URL;
+}
+
+if (process.env.TEST_PRYVLIB_DNSLESS_URL != null) {
+  coreDnsLessUrl = process.env.TEST_PRYVLIB_DNSLESS_URL;
+  serviceInfoUrl = coreDnsLessUrl + 'reg/service/info';
+}
 
 /**
  * Data used for tests
@@ -28,42 +39,28 @@ async function prepare () {
 
   // fetch serviceInfo
   const serviceInfo = (await superagent.get(serviceInfoUrl)).body;
-  if (serviceInfo.api == null) throw Error('Invalid service Info ' + JSON.stringify(serviceInfo));
-  // test if user exists
-  const userExists = (await superagent.get(serviceInfo.register + username + '/check_username')).body;
-  if (typeof userExists.reserved === 'undefined') throw Error('Invalid user exists ' + JSON.stringify(userExists));
+  if (serviceInfo.api == null) throw new Error('Invalid service Info ' + JSON.stringify(serviceInfo));
+  console.log('Testing on ' + serviceInfo.name + ' > register: ' + serviceInfo.register);
+  await getHost(serviceInfo);
+  if (!await testUserExists(serviceInfo, username)) { // create user
+    const host = await getHost(serviceInfo);
 
-  let hostingCandidate = null;
-  if (!userExists.reserved) { // create user
-    // get available hosting
-    const hostings = (await superagent.get(serviceInfo.register + 'hostings').set('accept', 'json')).body;
-    findOneHostingKey(hostings, 'N');
-    function findOneHostingKey (o, parentKey) {
-      for (const key of Object.keys(o)) {
-        if (parentKey === 'hostings') {
-          hostingCandidate = key;
-          return key;
-        }
-        if (typeof o[key] !== 'string') {
-          findOneHostingKey(o[key], key);
-        }
-      }
+    try {
+      // create user
+      await superagent.post(host + 'users')
+        .send({
+          appId: 'js-lib-test',
+          username: username,
+          password: username,
+          email: username + '@pryv.io',
+          invitationtoken: 'enjoy',
+          languageCode: 'en',
+          referer: 'test-suite'
+        });
+    } catch (e) {
+      console.log('Failed creating user ', e);
+      throw new Error('Failed creating user ' + host + 'users');
     }
-    if (hostingCandidate == null) throw Error('Cannot find hosting in: ' + JSON.stringify(hostings));
-    const hosting = hostingCandidate;
-
-    // create user
-    await superagent.post(serviceInfo.register + 'user')
-      .send({
-        appid: 'js-lib-test',
-        hosting: hosting,
-        username: username,
-        password: username,
-        email: username + '@pryv.io',
-        invitationtoken: 'enjoy',
-        languageCode: 'en',
-        referer: 'test-suite'
-      });
   }
   const apiEndpoint = serviceInfo.api.replace('{username}', username);
 
@@ -90,4 +87,53 @@ async function prepare () {
   const res = regexAPIandToken.exec(apiEndpoint);
   testData.apiEndpointWithToken = res[1] + '://' + testData.token + '@' + res[2];
   testData.apiEndpoint = apiEndpoint;
+}
+
+async function testUserExists (serviceInfo, username) {
+  if (coreDnsLessUrl != null) {
+    const coreApi = serviceInfo.api.replace('{username}', username);
+    try {
+      await superagent.get(coreApi + 'service/info');
+    } catch (e) {
+      if (e.response?.body?.error?.id === 'unknown-resource') {
+        return false;
+      }
+      throw new Error('Error testUserExists fetching: ' + coreApi + '/service/info');
+    }
+    return true;
+  }
+  // test if user exists
+  try {
+    const userExists = (await superagent.get(serviceInfo.register + username + '/check_username')).body;
+    if (typeof userExists.reserved === 'undefined') throw Error('Invalid user exists ' + JSON.stringify(userExists));
+    return userExists.reserved;
+  } catch (e) {
+    throw Error('Failed check user ' + serviceInfo.register + username + '/check_username', e);
+  }
+}
+
+async function getHost (serviceInfo) {
+  if (coreDnsLessUrl != null) {
+    return coreDnsLessUrl;
+  }
+  // get available hosting
+  const hostings = (await superagent.get(serviceInfo.register + 'hostings').set('accept', 'json')).body;
+  let hostingCandidate = null;
+  findOneHostingKey(hostings, 'N');
+  function findOneHostingKey (o, parentKey) {
+    for (const key of Object.keys(o)) {
+      if (parentKey === 'hostings') {
+        const hosting = o[key];
+        if (hosting.available) {
+          hostingCandidate = hosting;
+        }
+        return;
+      }
+      if (typeof o[key] !== 'string') {
+        findOneHostingKey(o[key], key);
+      }
+    }
+  }
+  if (hostingCandidate == null) throw Error('Cannot find hosting in: ' + JSON.stringify(hostings));
+  return hostingCandidate.availableCore;
 }
